c45b0ac14b7784003f5971da774d055fb6a91ae5
Arreguit <jonathan.arreguitoneill@epfl.ch>
2018-03-05 22:56:26 +0100

    Lab1 corrections

---
+++ Lab1/Corrections/Python/systems.py
@@ -0,0 +1,35 @@
+""" Systems related codes for exercise 2 (Only for corrections) """
+
+import numpy as np
+import biolog
+
+try:
+    import sympy
+    IMPORT_SYMPY = True
+except ImportError:
+    IMPORT_SYMPY = False
+
+
+def fixed_point(A):
+    """ Compute fixed point """
+    x = sympy.symbols(["x{}".format(i) for i in range(2)])
+    sol = sympy.solve(np.dot(A, x), x)
+    x0 = sol[x[0]], sol[x[1]]
+    biolog.info("Fixed point: {}".format(x0))
+    return
+
+
+def eigen(A):
+    """ Compute eigenvectors and eigenvalues """
+    eigenvalues, eigenvectors = np.linalg.eig(A)
+    biolog.info("Eigenvalues: {}".format(eigenvalues))
+    biolog.info("Eigenvectors:\n{}".format(eigenvectors))
+    return eigenvalues, eigenvectors
+
+
+def system_analysis(A):
+    """ Exercise 2.a - Analyse system """
+    if IMPORT_SYMPY:
+        fixed_point(A)
+        eigen(A)
+    return
