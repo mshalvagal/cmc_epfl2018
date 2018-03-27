3e227f284e8e69427e57250c818566328075b806
Matthieu <matthieu.dujany@epfl.ch>
2018-03-28 00:32:17 +0200

    modified ex1e plot

--- Lab4/Python/exercise1.py
+++ Lab4/Python/exercise1.py
@@ -27,12 +27,10 @@ def amplitude_experiments(parameters, x0_list, time):
         dtheta_amp[i] = np.max(dthetas,axis=0)-np.min(dthetas,axis=0)
     return [theta_amp/2,dtheta_amp/2]
 
-def equilibrium_func(k2,theta_ref2,parameters):
+def equilibrium_func(k2,theta_ref2,parameters,k1,theta_ref1):
     g=parameters.g
     L=parameters.L
     sin = parameters.sin
-    theta_ref1 = np.pi/6
-    k1=0#the value of 
     
     theta_eq = np.pi/6
     return g*sin(theta_eq)/L-k1*min(theta_ref1-theta_eq,0)-k2*max(theta_ref2-theta_eq,0)
@@ -42,21 +40,48 @@ def plot_solutions_eq(parameters):
     theta_ref2_range = np.arange(np.pi/6+0.01,np.pi/2,0.01)
     k2_sol = np.zeros([len(theta_ref2_range)])
     initial_guess = 1
-    for i,theta_ref2 in enumerate(theta_ref2_range):
-        sol = root(equilibrium_func,initial_guess,args=(theta_ref2,parameters),method='hybr')
-        k2_sol[i]=sol.x[0]
+    
+    legend_list =[]
     plt.figure()
+    
+    #first couple of values
+    theta_ref1 = np.pi/6
+    k1=0
+    for i,theta_ref2 in enumerate(theta_ref2_range):
+        sol = root(equilibrium_func,initial_guess,args=(theta_ref2,parameters,k1,theta_ref1),method='hybr')
+        k2_sol[i]=sol.x[0]
     plt.plot(theta_ref2_range,k2_sol)
+    legend_list.append(r"Solutions for $\theta_{ref1}=\frac{\pi}{6}$")
+    
+    theta_ref1 = 0
+    k1=20
+    for i,theta_ref2 in enumerate(theta_ref2_range):
+        sol = root(equilibrium_func,initial_guess,args=(theta_ref2,parameters,k1,theta_ref1),method='hybr')
+        k2_sol[i]=sol.x[0]
+    plt.plot(theta_ref2_range,k2_sol)
+    legend_list.append(r"Solutions for $\theta_{ref1}=0$, $K_1=20$")
+    
+    theta_ref1 = 0
+    k1=40
+    for i,theta_ref2 in enumerate(theta_ref2_range):
+        sol = root(equilibrium_func,initial_guess,args=(theta_ref2,parameters,k1,theta_ref1),method='hybr')
+        k2_sol[i]=sol.x[0]
+    plt.plot(theta_ref2_range,k2_sol)
+    legend_list.append(r"Solutions for $\theta_{ref1}=0$, $K_1=40$")
+    
+    #adding nice things for the graph
     plt.axvline(x=np.pi/6,color='k',linestyle='--')
     ax = plt.gca()
     #ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
     #ax.xaxis.set_major_locator(tck.MultipleLocator(base=0.2))
     ax.set_xticks([np.pi/6, np.pi/4, np.pi/3, np.pi*2/5,np.pi/2])
-    ax.set_xticklabels([r"$\frac{1}{6}\pi$",r"$\frac{1}{4}\pi$",
-                        r"$\frac{1}{3}\pi$",r"$\frac{2}{5}\pi$",r"$\frac{1}{2}\pi$"])
-    plt.xlabel("Value of theta_ref2 in radians")
-    plt.ylabel("Value of K2")
+    ax.set_xticklabels([r"$\frac{\pi}{6}$",r"$\frac{\pi}{4}$",
+                        r"$\frac{\pi}{3}$",r"$\frac{\pi}{5}$",r"$\frac{\pi}{2}$"])
+    plt.xlabel(r"Value of $\theta_{ref2}$ in radians")
+    plt.ylabel(r"Value of spring constant $K_2$")
+    #plt.title(r"Solution $\theta_{ref2}$ for an equilibrium position at $\frac{\pi}{6}$")
     plt.ylim((0,100))
+    plt.legend(legend_list)
     plt.show()
 
 
