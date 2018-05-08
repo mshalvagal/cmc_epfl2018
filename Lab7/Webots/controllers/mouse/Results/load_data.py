205b500a8a63b25e20f12a04da9c79d081d8d277
Delaux <37582482+Delaux@users.noreply.github.com>
2018-05-08 15:36:16 +0200

    lab7 plots

--- Lab7/Webots/controllers/mouse/Results/load_data.py
+++ Lab7/Webots/controllers/mouse/Results/load_data.py
@@ -1,6 +1,16 @@
 """ Load results data """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+# Global settings for plotting
+# You may change as per your requirement
+plt.rc('lines', linewidth=2.0)
+plt.rc('font', size=35.0)
+plt.rc('axes', titlesize=25.0)     # fontsize of the axes title
+plt.rc('axes', labelsize=22.0)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=20.0)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=20.0)    # fontsize of the tick labels
 
 
 def load_data():
@@ -9,14 +19,53 @@ def load_data():
     ankle_r_trajectory = np.load("ankle_r_trajectory.npy")
     return [ankle_l_trajectory, ankle_r_trajectory]
 
+def plot_trajectories():
+    """ Plot the trajectories of the hind feet"""
+    
+    ankle_l_trajectory, ankle_r_trajectory = load_data()
+    
+    time=np.linspace(1,len(ankle_l_trajectory[:,0]),len(ankle_l_trajectory[:,0]));
+    
+    plt.figure('Trajectories')
+    plt.subplot(311)
+    plt.plot(time,ankle_l_trajectory[:,0])
+    plt.plot(time,ankle_r_trajectory[:,0])
+    plt.title('Trajectory of the X component')
+    #plt.xlabel('Time [ms]')
+    plt.ylabel('Position [cm]')
+    plt.legend(['Left ankle','Right ankle'])
+    
+    plt.subplot(312)
+    plt.plot(time,ankle_l_trajectory[:,1])
+    plt.plot(time,ankle_r_trajectory[:,1])
+    plt.title('Trajectory of the Y component')
+    #plt.xlabel('Time [ms]')
+    plt.ylabel('Position [cm]')
+    plt.legend(['Left ankle','Right ankle'])
+    
+    plt.subplot(313)
+    plt.plot(time,ankle_l_trajectory[:,2])
+    plt.plot(time,ankle_r_trajectory[:,2])
+    plt.title('Trajectory of the Z component')
+    plt.xlabel('Time [ms]')
+    plt.ylabel('Position [cm]')
+    plt.legend(['Left ankle','Right ankle'])
+    
+    plt.suptitle('Decomposition of the trajectories of the hind feet')
+    return
+
 
 def main():
     """ Main """
-    ankle_l_trajectory, ankle_r_trajectory = load_data()
-    print("ankle_l_trajectory:\n{}\nankle_r_trajectory:\n{}".format(
-        ankle_l_trajectory,
-        ankle_r_trajectory
-    ))
+    
+    #ankle_l_trajectory, ankle_r_trajectory = load_data()
+    #print("ankle_l_trajectory:\n{}\nankle_r_trajectory:\n{}".format(
+    #    ankle_l_trajectory,
+    #    ankle_r_trajectory
+    #))
+    
+    plot_trajectories()
+    
     return
 
 
